c518667139bbb956dc2818bf67f1fa5a133aaf12
frenchsimu <discordtopinfos@gmail.com>
2022-02-25 15:12:09 +0100

    Ajout sécuritée admin_bot

--- commands/admin/emit.js
+++ commands/admin/emit.js
@@ -1,11 +1,13 @@
 const { MessageEmbed } = require('discord.js');
 const { required } = require('nodemon/lib/config');
 const guildMemberAdd = require('../../events/guild_members/guildMemberAdd');
+const admins = require('../../local storage/admins_bot.json')
 
 module.exports = {
 	name: 'emit',
 	description: 'Emettre un évènement au choix !',
 	run: (client, message, args) => {
+		if (!admins.admins.includes(message.author.id)) return;
 		if (!args[0] || !args[0].match(/^(guildMemberAdd|guildMemberRemove)$/)) return message.reply({ content: `Merci d'entrer un évènement valide (\`guildMemberAdd\`, \`guildMemberRemove\`)` });
 
 		if (args[0] == 'guildMemberAdd') {
@@ -35,6 +37,7 @@ module.exports = {
 		}
 	],
 	runSlash: (client, interaction) => {
+		if (!admins.admins.includes(interaction.user.id)) interaction.deferReply({ ephemeral: true })
 		const evtChoices = interaction.options.getString('event');
 
 		if (evtChoices == 'guildMemberAdd') {
