163277319549abfa44691b93bced56b993927ccb
frenchsimu <discordtopinfos@gmail.com>
2022-02-27 18:59:41 +0100

    Ajout => Clear + UserInfo + BotInfo + Invite

---
+++ commands/utils/BotInfos.js
@@ -0,0 +1,84 @@
+const { MessageEmbed } = require('discord.js')
+const package = require('../../package.json')
+
+module.exports = {
+	name: 'botinfo',
+	description: 'Donne des informations relatives au bot',
+	async run (client, message, args) {
+		let guilds = 0
+		let members = 0
+		client.guilds.cache.map(g => {
+			guilds = guilds + 1
+		})
+		client.guilds.members.cache.map(m => {
+			members = members + 1
+		})
+		
+		const embed = new MessageEmbed()
+			.setTitle('Informations')
+			.setDescription('Voici quelques informations me concernant :')
+			.addFields([
+				{
+					name: 'Version', value: `${package.version}`, inline: true
+				},
+				{
+					name: 'Serveurs', value: `${guilds}`, inline: true
+				},
+				{
+					name: 'Utilisateurs', value: `${members}`, inline: true
+				},
+				{
+					name: 'Site Web', value: `[Site web](https://www.google.com)`, inline: true
+				},
+			])
+		;
+		message.reply({embed: [embed]})
+
+	},
+	async runInteraction (client, interaction) {
+		
+		let guilds = 0
+		let members = 0
+		client.guilds.cache.map(g => {
+			guilds = guilds + 1
+			members = members + g.memberCount
+		})
+		
+		const embed = new MessageEmbed()
+			.setTitle('Informations')
+			.setDescription('Voici quelques informations me concernant :')
+			.addFields([
+				{
+					name: 'Version', value: `${package.version}`, inline: true
+				},
+				{
+					name: 'Serveurs', value: `${guilds}`, inline: true
+				},
+				{
+					name: 'Utilisateurs', value: `${members}`, inline: true
+				},
+				{
+					name: 'Shards', value: `1/1\n( Prévu aux 75 serveurs )`, inline: true
+				},
+				{
+					name: 'Site Web', value: `[Site web](https://frenchsimu.github.io/Code-Industry-website/)`, inline: true
+				},
+				{
+					name: 'Ajoute Moi', value: `[\[Invitation\]](https://discord.com/api/oauth2/authorize?client_id=902293972091801620&permissions=8&scope=bot%20applications.commands )`, inline: true
+				},
+				{
+					name: 'Support', value: `[\[Invitation\]](http://www.discord.gg/tCmb8yGZYw)`, inline: true
+				},
+				{
+					name: 'Développeur', value: `Achille - Dev`, inline: true
+				}
+			])
+			.setTimestamp()
+			.setFooter({
+				text: 'Achille - Dev', iconURL: `${client.user.displayAvatarURL()}`
+			})
+		;
+		interaction.reply({embeds: [embed], ephemeral: true})
+
+	}
+};
---
+++ commands/utils/invite.js
@@ -0,0 +1,42 @@
+const { MessageEmbed, Message, MessageActionRow, MessageButton } = require('discord.js')
+
+module.exports = {
+	name: 'invite',
+	description: 'Donne le lien d\'invitation du bot',
+	async run (client, message, args) {
+
+		const embed = new MessageEmbed()
+			.setTitle('Inviter le bot')
+			.setURL('https://discord.com/api/oauth2/authorize?client_id=902293972091801620&permissions=8&scope=bot%20applications.commands')
+			.setColor('#4ed5f8')
+		;
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setLabel('Inviter')
+					.setStyle('LINK')
+					.setURL('https://discord.com/api/oauth2/authorize?client_id=902293972091801620&permissions=8&scope=bot%20applications.commands')
+			)
+		;
+		message.reply({ embeds: [embed], components: [row] })
+		
+	},
+	async runInteraction (client, interaction) {
+		
+		const embed = new MessageEmbed()
+		.setTitle('Inviter le bot')
+		.setURL('https://discord.com/api/oauth2/authorize?client_id=902293972091801620&permissions=8&scope=bot%20applications.commands')
+		.setColor('#4ed5f8')
+	;
+	const row = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setLabel('Inviter')
+				.setStyle('LINK')
+				.setURL('https://discord.com/api/oauth2/authorize?client_id=902293972091801620&permissions=8&scope=bot%20applications.commands')
+		)
+	;
+	interaction.reply({ embeds: [embed], components: [row], ephemeral: true })
+
+	}
+};
