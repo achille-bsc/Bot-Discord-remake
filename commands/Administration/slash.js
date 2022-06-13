f68215d2d69be826969f37e497214f4c1584df7c
achille-bsc <discordtopinfos@gmail.com>
2022-06-13 11:57:30 +0200

    test push corruption

--- commands/Administration/slash.js
+++ commands/Administration/slash.js
@@ -20,65 +20,29 @@ module.exports = {
 			type: 'SUB_COMMAND'
 		}
 
-<<<<<<< HEAD
-  ],
-  async runInteraction (client, interaction) {
-    // const guild = await client.getGuild(interaction.guild)
-    // const lang = guild.langue === 'fr' ? langFr : langEn
-=======
 	],
 	async runInteraction (client, interaction) {
-		const guild = await client.getGuild(interaction.guild)
-		const lang = guild.langue === 'fr' ? langFr : langEn
->>>>>>> 61309eb7478fef3ea2721e9c1f7333cc3d3f18e0
+		// const guild = await client.getGuild(interaction.guild)
+		// const lang = guild.langue === 'fr' ? langFr : langEn
 
-<<<<<<< HEAD
 		if (interaction.options.getSubcommand() === 'update') {
 			const guildObject = await client.guilds.cache.get(interaction.guild.id)
-			await guildObject.commands.set(client.commands.map(cmd => cmd))
-			const embed = new MessageEmbed()
-				.setTitle(`${lang.trueTitle}`)
-				.setColor('GREEN')
-				.setFooter({ text: `${lang.footer} ${interaction.member.tag}`, avatarURL: `${interaction.member.displayAvatarURL(true)}` })
-			await interaction.reply({ embeds: [embed] })
+			interaction.deferReply()
+			interaction.deleteReply()
+			await guildObject.commands.set(client.commands.map(cmd => cmd)).then(async () => {
+				const embed = new MessageEmbed()
+					.setTitle('Slash commandes Mises à jours')
+					.setColor('GREEN')
+				await interaction.editReply({ embeds: [embed] })
+			})
 		} else if (interaction.options.getSubcommand() === 'off') {
-			interaction.guild.commands.set([])
-			const embed = new MessageEmbed()
-				.setTitle(`${lang.falseTitle}`)
-				.setColor('RED')
-=======
-    if (interaction.options.getSubcommand() === 'update') {
-      const guildObject = await client.guilds.cache.get(interaction.guild.id)
-      interaction.deferReply()
+			interaction.guild.commands.set([]).then(() => {
+				const embed = new MessageEmbed()
+					.setTitle('Les slash commands ont correctement été désactivés !')
+					.setColor('RED')
 
-      await guildObject.commands.set(client.commands.map(cmd => cmd)).then(async () => {
-        const embed = new MessageEmbed()
-          .setTitle('Slash commandes Mises à jours')
-          .setColor('GREEN')
-        await interaction.editReply({ embeds: [embed] })
-      })
-    } else if (interaction.options.getSubcommand() === 'off') {
-<<<<<<< HEAD
-      interaction.deferReply()
-
-      interaction.guild.commands.set([]).then(() => {
-        const embed = new MessageEmbed()
-          .setTitle('Les slash commands ont correctement été désactivés !')
-          .setColor('RED')
-
-        interaction.repeditReokyly({ embeds: [embed], ephemeral: true })
-      })
-    }
-  }
-=======
-      interaction.guild.commands.set([])
-      const embed = new MessageEmbed()
-        .setTitle(`${lang.falseTitle}`)
-        .setColor('RED')
->>>>>>> 27652c02e5c049b01c6fbd3b75c66f0d6162182f
-
-			interaction.reply({ embeds: [embed], ephemeral: true })
+				interaction.reply({ embeds: [embed], ephemeral: true })
+			})
 		}
 	}
->>>>>>> 61309eb7478fef3ea2721e9c1f7333cc3d3f18e0
 }
